7fb78b30a030ee2f2028949bdbbccca3b08fbc29
ihsan YILMAZ <124254928+Arkihek@users.noreply.github.com>
2023-09-07 10:55:18 +0300

    Day 07

--- src/test/java/testdatas/JSONPlaceData.java
+++ src/test/java/testdatas/JSONPlaceData.java
@@ -2,6 +2,9 @@ package testdatas;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class JSONPlaceData {
     public int basariliStatusCode = 200;
     public String contentType = "application/json; charset=utf-8";
@@ -24,6 +27,19 @@ public class JSONPlaceData {
         reqBody.put("body", "Merhaba");
         reqBody.put("userId", 10);
         reqBody.put("id", 70);
+
+        return reqBody;
+    }
+
+
+    public HashMap<String, Object> reqBodyOlusturMAP(){
+        HashMap<String, Object> reqBody = new HashMap<>();
+
+        reqBody.put("title","Ahmet");
+        reqBody.put("body","Merhaba");
+        reqBody.put("userId",10.0);
+        reqBody.put("id",70.0);
+
 
         return reqBody;
     }
